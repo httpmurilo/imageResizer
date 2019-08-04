c91d9f46a52d847125e8d8fb64db307a2cb37111
Murilo Eduardo Saldanha R de Melo <muriloeduardo1997@outlook.com>
2019-08-04 19:27:02 -0300

    Criação dos models,converter

---
+++ imageresize/Models/ResizeRequestModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace imageresize.Models
+{
+    public class ResizeRequestModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Format { get; set; }
+        public int? Height { get; set; }
+        public int? Width { get; set; }
+        public int? Quality { get; set; }
+    }
+}
